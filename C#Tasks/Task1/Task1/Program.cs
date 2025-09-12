6396b7202f571adb7e806c4b787a0483efc58dbb
Elgammal-42 <152219728+Elgammal-42@users.noreply.github.com>
2025-09-12 22:45:12 +0300

    Add files via upload

---
+++ C#Tasks/Task1/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*#region charToAscii
+            Console.WriteLine("Enter one character");
+            char c = char.Parse(Console.ReadLine());
+            Console.WriteLine($"Character ascii is: {(int)c}");
+            #endregion*/
+
+            /*#region asciiToChar
+            Console.WriteLine("Enter ASCII of the character");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Character is: {(char)i}");
+            #endregion*/
+
+            /*#region oddOrEven
+            Console.WriteLine("Enter number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your number is: {(num % 2 == 1 ? "odd" : "even")}");
+            #endregion*/
+
+            /*#region operations
+            Console.WriteLine("Enter two numbers");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Numbers sum = {num1 + num2}, subtraction = {num1 - num2}, multiplication = {num1 * num2}");
+            #endregion*/
+
+            /*Console.WriteLine("Enter your degree");
+            int degree = int.Parse(Console.ReadLine());
+            if (degree >= 90)
+                Console.WriteLine("A");
+            else if (degree >= 80)
+                Console.WriteLine("B");
+            else if (degree >= 70)
+                Console.WriteLine("C");
+            else if (degree >= 60)
+                Console.WriteLine("D");
+            else
+                Console.WriteLine("F");*/
+
+            /*for (int i = 1; i < 13; i++)
+            {
+                for (int j = 1; j < 13; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }*/
+
+        }
+    }
+}
