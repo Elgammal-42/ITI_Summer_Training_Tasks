03aaefb1e8d74030d1ec659c3b2e090b2d0c5c0a
Elgammal-42 <152219728+Elgammal-42@users.noreply.github.com>
2025-09-26 18:28:22 +0300

    Add files via upload

---
+++ EF/Task-1/Task-1/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Subject
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Subject[] Subjects { get; set; } = null!;
+
+    }
+}
